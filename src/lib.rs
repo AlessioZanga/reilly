8794d186361d8e0ff3ae3dec0d1df8c13f387248
AlessioZanga <alessio.zanga@outlook.it>
2022-05-27 09:13:59 +0200

    Update root module

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,11 @@
 //! A REInforcement Learning LibrarY.
 
 /// Generic reinforcement learning agents.
+/// Collection of agent's policies.
+pub mod policies;
 
 /// Collection of value functions.
 pub mod values;
+
+/// Definition of fundamental types.
+pub mod types;
